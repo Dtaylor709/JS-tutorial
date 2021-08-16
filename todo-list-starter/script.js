c4bad612eea30ab21f4e0214209d0a955e73ffec
Dtaylor709 <84775234+Dtaylor709@users.noreply.github.com>
2021-08-16 22:49:42 +0800

    Initial commit

---
+++ todo-list-starter/script.js
@@ -0,0 +1,14 @@
+var todoItems = [
+    {
+        text: "Example",
+        complete: false,
+    },
+];
+
+function addTodoItem() {}
+
+function updateTodoList() {}
+
+function createNewTodoItemElement(todoItem, index) {}
+
+function markComplete(index) {}
