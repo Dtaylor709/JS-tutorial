3e20820ed53dec3560d29ed841e925461338b058
Dtaylor709 <84775234+Dtaylor709@users.noreply.github.com>
2021-08-16 23:17:53 +0800

    Update script.js

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -1,1 +1,23 @@
 console.log("Hello, world!");
+alert("Hello, world!");
+let username = "Hayley";
+console.log("Hi, " + username);
+
+let num1 = 3;
+let num2 = 4;
+let total = num1 + num2;
+console.log(total);
+
+
+function addition(x, y) {
+    result = x + y;
+    console.log(result);
+    }
+
+addition(1, 9);
+
+function showName() {
+    console.log("Cool name!");
+    let username = document.getElementById("name-text").value;
+    console.log("Hi, " + username)  
+    }
