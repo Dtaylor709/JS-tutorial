9da35cb6a1599e2e8d7123a652fb45890fc5e4d8
Dtaylor709 <84775234+Dtaylor709@users.noreply.github.com>
2021-08-18 20:30:41 +0800

    Challenge

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -4,16 +4,23 @@
 //         complete: false,
 //     },
 // ];
-let todoTasksText = ['walk Chilli', 'make dinner'];
-let todoTasksStatus = [false, true];
+let todoTasksText = [];
+let todoTasksStatus = [];
+let todoDates = [];
+let important = [];
 updateTodoList();
+
+
 
 function addTodoItem() { 
     let newTask = document.getElementById('new-task-text');
-    if (newTask.value) {
+    let newDate = document.getElementById('new-task-date');
+    if (newTask.value && newDate.value) {
         todoTasksText.push(newTask.value);
         todoTasksStatus.push(false);
+        todoDates.push(newDate.value)
         newTask.value = '';
+        newDate.value = '';
     }
     updateTodoList();
 }
@@ -37,12 +44,15 @@ function createNewTodoItemElement(task, index) {
     let paragraph = document.createElement('p');
     //      set the inner text of the paragraph to the task text
     paragraph.innerText = task;
-
-    if (todoTasksStatus[index] == true) {
-        paragraph.classList.add('complete');
-    }
+  
     //      create a new list item (<li>) element
     let listItem = document.createElement('li');
+    if (todoTasksStatus[index] == true) {
+        listItem.classList.add('complete');
+    }
+    else {
+        listItem.classList.add('incomplete')
+    }
     //      add the paragraph tag as a child of the li element
     listItem.appendChild(paragraph);
     console.log(listItem);
@@ -53,7 +63,26 @@ function createNewTodoItemElement(task, index) {
     completeButtonElement.onclick = function () {
         markComplete(index)
     };
+    let importantButtonElement = document.createElement('input');
+    importantButtonElement.type = 'button';
+    importantButtonElement.value = 'important';
+    importantButtonElement.onclick = function () {
+        markImportant(index)
+    }
+    listItem.appendChild(importantButtonElement)
+
+    if (important[index] == true) {
+        let importantPic = document.createElement('img')
+        importantPic.src = 'https://img.resized.co/entertainment/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvczMtZXUtd2VzdC0xLmFtYXpvbmF3cy5jb21cXFwvZW50ZXJ0YWlubWVudGllXFxcL3N0b3JhZ2VcXFwvaW1hZ2VzX2NvbnRlbnRcXFwvcmVjdGFuZ2xlXFxcLzYyMHgzNzJcXFwvU2NyZWVuLVNob3QtMjAxNi0wOS0xMC1hdC0xNzU5MzgucG5nXCIsXCJ3aWR0aFwiOjEyMDAsXCJoZWlnaHRcIjo2MzAsXCJkZWZhdWx0XCI6XCJodHRwczpcXFwvXFxcL2VudGVydGFpbm1lbnQuaWVcXFwvaW1hZ2VzXFxcL25vLWltYWdlLnBuZ1wifSIsImhhc2giOiJkYjJjMWVlNmI1ZDIyYzRjZGE4ZTQ5MDk4ODZiYWMxZjZlNmNiNzRkIn0=/pic-this-picture-of-terry-crews-can-help-save-you-money.png'
+        listItem.appendChild(importantPic)
+    }
+    
+    let dateElement = document.createElement('p');
+    dateElement.innerText = todoDates[index];
+    listItem.appendChild(dateElement);
+    
     listItem.appendChild(completeButtonElement);
+
 
     return listItem
 }
@@ -66,5 +95,15 @@ function markComplete(index) {
     }
     console.log(todoTasksStatus);
     updateTodoList();
-    }
+}
 
+
+function markImportant(index) {
+    if (important[index] == false) {
+        important[index] = true;
+    } else {
+        important[index] = false;
+    }
+    console.log(important);
+    updateTodoList();
+}
